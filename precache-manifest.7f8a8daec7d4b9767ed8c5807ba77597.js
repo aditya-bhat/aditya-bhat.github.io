1859b4b2075694b1af7c46607ad9206740538345
Aditya Bhat <adityadb24@gmail.com>
2020-08-16 20:22:12 +0530

    Updates

--- precache-manifest.7f8a8daec7d4b9767ed8c5807ba77597.js
+++ precache-manifest.7f8a8daec7d4b9767ed8c5807ba77597.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6015a0083d7c3e349faf60d3189ca1b0",
+    "revision": "9bc22e2cbe3d789736490eb0a6ec3a50",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
+    "revision": "683ec9cef7fb9df849d0",
     "url": "/portfolio/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
-    "url": "/portfolio/static/js/main.10a1b4a7.chunk.js"
+    "revision": "683ec9cef7fb9df849d0",
+    "url": "/portfolio/static/js/main.d7f8582d.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
