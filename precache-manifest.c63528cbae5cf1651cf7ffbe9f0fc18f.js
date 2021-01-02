17c76505e6062eba3e0c6a44300d2a501548050d
Aditya Bhat <adityadb24@gmail.com>
2021-01-02 22:25:09 +0530

    Updates

---
+++ precache-manifest.c63528cbae5cf1651cf7ffbe9f0fc18f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059823f2c4a22873b2ffdc0ecb8c932f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1a2a2469ca58fe2da7",
+    "url": "/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "ebc5427b34fa151da96f",
+    "url": "/static/css/main.2bc7dd2e.chunk.css"
+  },
+  {
+    "revision": "5d1a2a2469ca58fe2da7",
+    "url": "/static/js/2.3a97e95e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.3a97e95e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc5427b34fa151da96f",
+    "url": "/static/js/main.897eb2bc.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "3c5b5034187f45e76bd5e216897b07c5",
+    "url": "/static/media/profile.3c5b5034.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62610f47abce4fe183d7cb478bbc6a09.js"
+  "/precache-manifest.c63528cbae5cf1651cf7ffbe9f0fc18f.js"
 );
 
 self.addEventListener('message', (event) => {
