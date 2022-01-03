2df37485ec8878d4325e56e5c8186d8f8fb8a28a
Aditya Bhat <adityadb24@gmail.com>
2022-01-03 15:46:11 -0700

    Updates

---
+++ precache-manifest.2ba21733aea8fdad8b5be1b6492e17db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139d24f3042442b2ccdb359d669c5fc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2936629cf0c7688eb5c",
+    "url": "/static/css/2.5180306a.chunk.css"
+  },
+  {
+    "revision": "ab3eb6d33c59f06c1ffa",
+    "url": "/static/css/main.11369312.chunk.css"
+  },
+  {
+    "revision": "f2936629cf0c7688eb5c",
+    "url": "/static/js/2.bd2078b6.chunk.js"
+  },
+  {
+    "revision": "70b070bc4c4b879d3d3b9683ea8bec9f",
+    "url": "/static/js/2.bd2078b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab3eb6d33c59f06c1ffa",
+    "url": "/static/js/main.99eb5b9c.chunk.js"
+  },
+  {
+    "revision": "657e5de65364a585ec60",
+    "url": "/static/js/runtime-main.f24722ca.js"
+  },
+  {
+    "revision": "3c5b5034187f45e76bd5e216897b07c5",
+    "url": "/static/media/profile.3c5b5034.png"
+  },
+  {
+    "revision": "b5594cf82447e91b2f4c23638247df32",
+    "url": "/static/media/work.b5594cf8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f00cdb593ecee2f64eb7853b249438e8.js"
+  "/precache-manifest.2ba21733aea8fdad8b5be1b6492e17db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
